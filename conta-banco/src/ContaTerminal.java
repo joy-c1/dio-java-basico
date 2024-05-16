053a2a32f3433bc297f084cc1bc91b06d9343ee7
joy-c1 <joyce.freitas@estudante.ifgoiano.edu.br>
2024-05-16 17:00:15 -0300

    Importação da classe Scanner

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe Scanner
+
+        //Exibir as mensagens  para o nosso usuário
+
+        //Obter pela Scanner os valores digitados no terminal
+
+        //Exibir a mensagem da conta criada
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Bem vindo ao Banco Digital! Para prosseguir, necessitaremos de algumas informações...");
+        System.out.println("Por favor, insira seu nome: ");
+        String nome = scan.nextLine();
+        
+
     }
 }
