edee5ee77090ecdc2ecd5264a4dc6fd47a1b7246
joy-c1 <joyce.freitas@estudante.ifgoiano.edu.br>
2024-05-17 09:28:33 -0300

    Classe ContaTerminal finalizada, programa funcionando conforme esperado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -11,10 +11,20 @@ public class ContaTerminal {
         //Exibir a mensagem da conta criada
 
         Scanner scan = new Scanner(System.in);
+
         System.out.println("Bem vindo ao Banco Digital! Para prosseguir, necessitaremos de algumas informações...");
         System.out.println("Por favor, insira seu nome: ");
-        String nome = scan.nextLine();
-        
+        String nome = scan.next();
+        System.out.println("Por favor, insira o número da conta: ");
+        int numeroConta = scan.nextInt();
+        System.out.println("Por favor, insira o número da Agência:");
+        String agencia = scan.next();
+        System.out.println("Por favor, insira o saldo: ");
+        double saldo = scan.nextDouble();
+
+        scan.close();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo R$" + saldo + " já está disponível para saque.");
 
     }
 }
